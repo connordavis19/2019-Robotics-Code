3757d9b3f48cee1b70a06b2fe77435398fe01f80
Robotics Laptop 2 <cdavis19@setoncatholic.org>
2019-03-11 14:14:07 -0700

    Made the ladder work

--- src/main/java/frc/robot/commands/ElevatorDownCom.java
+++ src/main/java/frc/robot/commands/ElevatorDownCom.java
@@ -30,7 +30,7 @@ public class ElevatorDownCom extends Command {
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return false;
+    return !Robot.elevatorSub.getBottomLimit();
   }
 
   // Called once after isFinished returns true
--- src/main/java/frc/robot/commands/ElevatorUpCom.java
+++ src/main/java/frc/robot/commands/ElevatorUpCom.java
@@ -30,7 +30,7 @@ public class ElevatorUpCom extends Command {
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return false;
+    return !Robot.elevatorSub.getTopLimit();
   }
 
   // Called once after isFinished returns true

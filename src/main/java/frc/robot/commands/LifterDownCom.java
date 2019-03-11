c873ea104744182b425569a9cb535490d5e0e02f
James Heinemann <jheinemann20@setoncatholic.org>
2019-03-11 14:23:37 -0700

    minor comments

--- src/main/java/frc/robot/commands/LifterDownCom.java
+++ src/main/java/frc/robot/commands/LifterDownCom.java
@@ -30,7 +30,7 @@ public class LifterDownCom extends Command {
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return false;
+    return false; // limit switch code in LifterSub (so they will stop independantly)
   }
 
   // Called once after isFinished returns true
